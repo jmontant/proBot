d7dbeeadbc5e5c09fdae73a7b0952c967e4133b3
pbammel <bammelpj@hotmail.com>
2014-10-14 20:17:58 -0400

    General robot definitions

---
+++ robot_defs.h
@@ -0,0 +1,20 @@
+// Absolutes
+#define   TRUE      1
+#define   FALSE     0
+
+// Robot Action Words
+#define   ACTION    0
+#define   MOVE      1
+#define   TURN      2
+#define   RUN       3
+#define   LOOK      4
+#define   ATTACK    5
+#define   STOP      6
+#define   HELLO     7
+  // robot Direction words
+#define   LEFT      0
+#define   RIGHT     1
+#define   UP        2
+#define   DOWN      3
+#define   FORWARD   4
+#define   BACKWARD  5
